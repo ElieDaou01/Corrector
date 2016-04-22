e3fc78a1a192976e328f1fd7964385ffd514a13f
Elie <elied_9@hotmail.com>
2016-04-22 22:53:08 +0300

    User proofing

--- Dictionary_scripting_Perl.pl
+++ Dictionary_scripting_Perl.pl
@@ -1,7 +1,4 @@
 #!/usr/bin/perl
-#THINGS TO STILL DO:
-#ADD FEATURES, POLISH CODE, OPTIMIZE 
-#PLEASE REMEMBER THAT DICTIONARY FILE SHOULD BE PRESENT IN CURRENT DIRECTORY WHEN RUNNING SCRIPT
 
 use strict;
 use warnings;
@@ -9,8 +6,8 @@ use Text::LevenshteinXS qw(distance);		#library used to calculate levenstein dis
 use Time::HiRes qw(gettimeofday);		#library used to calculate code run-time (standard perl lib)
 my %hasher;
 my $start=gettimeofday();
-open(READER,"dictionary.txt") || die;	#dictionary file name. Included in send file.
-while (my $line = <READER>)		#Inserts dictionary file into hash
+open(READER,"dictionary.txt") || die;
+while (my $line = <READER>)
 {
 	chomp $line;
 	$hasher{$line}=1;
@@ -22,12 +19,12 @@ if(defined($ARGV[0]))
 }
 else
 {
-	open (READER, input());				#Takes input file from command argument or from user 
+	open (READER, input());
 }
-print "Please name output file name(Including file extension): \n";		#user inputed output file name as well as extension
+print "Please name output file name(Including file extension): \n";
 my $filen= <STDIN>;
 chomp $filen;
-open(WRITER,">$filen") || die "Output file writer failed" ;	#output file writer
+open(WRITER,">$filen") || die "Output file writer failed" ;
 my $mind;
 my $right;
 my $capital=0;
@@ -37,7 +34,7 @@ while(my $line = <READER>)
 	chomp $line;
 	my @words = split(" ",$line);
 	my $_ = $words[0];		#Using my $_ returns a warning depending on perl version and might not work with older versions. 
-	if(!(/^[A-Z]/))			#Check capitalization at start of new line
+	if(!(/^[A-Z]/))
 	{
 		print "\n$_ is not capitalized it should be: " . ucfirst($_) . "\n";
 		$line=ucfirst($line);
@@ -51,7 +48,7 @@ while(my $line = <READER>)
 	{
 		my @nearest;
 		$_ = $check;
-		if(/^\W/)		#Check for non word character at the start of the string
+		if(/^\W/)
 		{
 			$_= substr $check,1;
 			$check = substr $check,1;
@@ -65,14 +62,14 @@ while(my $line = <READER>)
 		else
 		{
 				$capital=0 if(/^[A-Z]/ and $capital);	
-				if(!(/^[A-Z]/) and $capital)		#Capitalization check
+				if(!(/^[A-Z]/) and $capital)
 				{
 					print "\n$check is not capitalized it should be: " . ucfirst($check) . "\n";
 					$right=ucfirst($check);
 					$line=~ s/([.?!]) $check/$1 $right/;
 					$capital=0;
 				}
-				if(/[.?!]$/)			#Checks for characters that require a capital letter afterwards
+				if(/[.?!]$/)
 				{
 					chop($check);
 					$capital=1;
@@ -87,9 +84,9 @@ while(my $line = <READER>)
 				$check = lc $check;
 				if(!(exists $hasher{$check}))
 				{
-					my @length= split(//, $check); 		#In order to get word size
+					my @length= split(//, $check);
 					my $wordL=$#length;
-					@nearest= ([$wordL,"No Suggestion found"],[$wordL,"No Suggestion found"],[$wordL,"No Suggestion found"],[$wordL,"No Suggestion found"],[$wordL,"No Suggestion found"]); 	# 2D array containing nearest 5 words and their respective distance, initialzed to word length. 
+					@nearest= ([$wordL,"No Suggestion found"],[$wordL,"No Suggestion found"],[$wordL,"No Suggestion found"],[$wordL,"No Suggestion found"],[$wordL,"No Suggestion found"]); 
 					print "\nWRONG WORD FOUND: $check\n";
 					print "Possible replacements: (Best to Worst)\n";
 					foreach my $key (keys %hasher)
@@ -98,7 +95,7 @@ while(my $line = <READER>)
 						if($temp<$nearest[4][0])
 							{
 								$nearest[4]=[$temp,$key];					
-								@nearest= sort { $a->[0] <=> $b->[0] } @nearest;		#sort array based on distance using value of nested array
+								@nearest= sort { $a->[0] <=> $b->[0] } @nearest;
 							}
 					}
 					foreach my $i (0..4)
@@ -108,7 +105,7 @@ while(my $line = <READER>)
 					if($nearest[0][1] ne "No Suggestion found")
 					{
 						$right=$nearest[0][1];
-						if($wrongcap)			#Checks for capitalization if the word is wrong (for output file)
+						if($wrongcap)
 						{	
 							$right=ucfirst($right);
 							$check=ucfirst($check);
@@ -134,13 +131,13 @@ sub input
 	print "Please enter input file name or filepath: \n";
 	my $filen = <STDIN>;
 	chomp $filen;
-	while (!(-e $filen)) 		#checks if file exists
+	while (!(-e $filen))
 	{
 		print "File does not exist please enter again: \n";
 		$filen = <STDIN>;
 		chomp $filen;
 	}
-	while (!(-r $filen)) 		#checks if file readable
+	while (!(-r $filen))
 	{
 		print "File is not readable please enter again: \n";
 		$filen = <STDIN>;
